884ab89e56e341f02e2bed21d1a16a368d4a7de2
ptrnov <ptr.nov@gmail.com>
2017-04-28 08:07:55 +0700

    piter

--- frontend/backend/master/models/AllStoreItemSearch.php
+++ frontend/backend/master/models/AllStoreItemSearch.php
@@ -88,7 +88,21 @@ class AllStoreItemSearch extends Model
         }
     }
 	
-	
+	public function addConditionX(Filter $filter, $attribute, $partial = false)
+    {
+        $value = $this->$attribute;
+
+        if (mb_strpos($value, '>') !== false) {
+            $value = intval(str_replace('>', '', $value));
+            $filter->addMatcher($attribute, new matchers\GreaterThan(['value' => $value]));
+
+        } elseif (mb_strpos($value, '<') !== false) {
+            $value = intval(str_replace('<', '', $value));
+            $filter->addMatcher($attribute, new matchers\LowerThan(['value' => $value]));
+        } else {
+            $filter->addMatcher($attribute, new matchers\SameAs(['value' => $value, 'partial' => $partial]));
+        }
+    }
 	
 	
 	
