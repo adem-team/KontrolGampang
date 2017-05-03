2b39c243dd7396dff12cd058091ec3fa2bc844fb
ptrnov <ptr.nov@gmail.com>
2017-05-03 11:37:37 +0700

    piter

--- frontend/backend/sales/views/all-trans/_formSearchStore.php
+++ frontend/backend/sales/views/all-trans/_formSearchStore.php
@@ -2,9 +2,24 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use kartik\widgets\DatePicker;
+use kartik\widgets\Select2;
+use yii\web\JsExpression;
+use yii\helpers\ArrayHelper;
+
+use common\models\Store;
+use common\models\StoreSearch;
+
+//$qryStore = Store::find()->select('OUTLET_CODE')->Where('FIND_IN_SET("'.Yii::$app->getUserOpt->user()['ACCESS_UNIX'].'", ACCESS_UNIX)');
+$qryStore = Store::find()->Where('FIND_IN_SET(ACCESS_UNIX,"'.Yii::$app->getUserOpt->user()['ACCESS_UNIX'].'")')->all();
+$valStoreMap = ArrayHelper::map($qryStore, 'OUTLET_CODE', 'OUTLET_NM');
+$urlx = \yii\helpers\Url::to(['/master/satuan-select']);
+$searchModel = new StoreSearch(['ACCESS_UNIX'=>Yii::$app->getUserOpt->user()['ACCESS_UNIX']]);
+			$dataProvider1 = $searchModel->searchUserStore(Yii::$app->request->queryParams);
+			$modelMenu=$dataProvider1->getModels();
+print_r($valStoreMap);
 ?>
 
-<div class="col-lg-12" style="padding-top:10px;margin-button:0px; height:35px; float:left">
+<div class="col-lg-4" style="padding-top:10px;margin-button:0px; height:35px; float:left">
 <!--<div class="col-lg-3">!-->
 	<?php $form = ActiveForm::begin([
 			'id' => $model->formName().'store',
@@ -13,20 +28,22 @@ use kartik\widgets\DatePicker;
 		]); ?>
 	 
 		<?php //=$form->field($model, 'TGL')->textInput([]) ?>
-		<?=$form->field($model, 'TGL')->widget(DatePicker::classname(), [
-						'options' => ['id'=>'form-store','placeholder' => 'Cari Tanggal'],
-						'type' => DatePicker::TYPE_BUTTON,
-							'pluginOptions' => [
-								'todayHighlight' => true,
-								'autoclose'=>true,
-								'format' => 'yyyy-m-dd'						
-							],
-							'pluginEvents'=>[
-								'show' => "function(e) {show}",
-							],
-						])->label(false);
-			?>	
-	 
+		<?= $form->field($model, 'OUTLET_ID')->widget(Select2::classname(), [
+					'data' => $valStoreMap,
+					'options'=>['placeholder'=>'Select ...'],
+					'pluginOptions' => [
+						'allowClear' => true,
+						// 'ajax' => [
+							// 'url' =>$urlx,
+							// 'dataType' => 'json',
+							// 'data' => new JsExpression('function(params) { 
+									// return {q:params.term,outlet_code:0001}; 
+								// }
+							// ')
+						// ],
+					],
+				])->label(false);
+			?>
 	   
 	 
 	<?php ActiveForm::end(); ?>
