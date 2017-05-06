a419a2cd3dfb14b74c6d463c38d20f53bb20c2a8
ptrnov <ptr.nov@gmail.com>
2017-05-06 14:12:36 +0700

    piter

--- frontend/backend/master/views/formula/_indexFormulaHarga.php
+++ frontend/backend/master/views/formula/_indexFormulaHarga.php
@@ -15,48 +15,131 @@ use kartik\widgets\ActiveForm;
 use kartik\tabs\TabsX;
 use kartik\date\DatePicker;
 use yii\web\View;
+use yii\web\Request;
+
 use frontend\backend\master\models\ItemJual;
 use frontend\backend\master\models\ItemJualSearch;
 
-	$searchModel = new ItemJualSearch();
+	$searchModel = new ItemJualSearch(['ITEM_ID'=>$paramCariItem]);
     $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-?>
-<div style="min-height:500px; font-family: tahoma ;font-size: 9pt;">
+	$bColorHarga='rgba(87,114,111, 1)';
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Item Jual', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
-		'id'=>'gv-formula-harga',
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            //'ID',
-            //'CREATE_BY',
-            //'CREATE_AT',
-            //'UPDATE_BY',
-            //'UPDATE_AT',
-            // 
-            // 'ITEM_ID',
-            // 'OUTLET_CODE',
-             'PERIODE_TGL1',
-             'PERIODE_TGL2',
-             'START_TIME',
-             'HARGA_JUAL',
-			 'STATUS',
-            // 'DCRIPT:ntext',
-
-            //['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
-</div>
-
+	$gvAttHarga=[
+		[
+			'class'=>'kartik\grid\SerialColumn',
+			'contentOptions'=>['class'=>'kartik-sheet-style'],
+			'width'=>'10px',
+			'header'=>'No.',
+			'headerOptions'=>Yii::$app->gv->gvContainHeader('center','30px',$bColorHarga,'#ffffff'),
+			'contentOptions'=>Yii::$app->gv->gvContainBody('center','30px',''),
+		],
+		//ITEM NAME
+		[
+			'attribute'=>'ITEM_ID',
+			//'label'=>'Cutomer',
+			'filterType'=>true,
+			'filterOptions'=>Yii::$app->gv->gvFilterContainHeader('0','50px'),
+			'hAlign'=>'right',
+			'vAlign'=>'middle',
+			'mergeHeader'=>false,
+			'noWrap'=>false,
+			//gvContainHeader($align,$width,$bColorHarga)
+			'headerOptions'=>Yii::$app->gv->gvContainHeader('center','50px',$bColorHarga),
+			'contentOptions'=>Yii::$app->gv->gvContainBody('left','50px',''),
+			
+		]	
+		,//HARI
+		[
+			'attribute'=>'PERIODE_TGL1',
+			'filterType'=>true,
+			'filterOptions'=>Yii::$app->gv->gvFilterContainHeader('0','50px'),
+			'hAlign'=>'right',
+			'vAlign'=>'middle',
+			'mergeHeader'=>false,
+			'noWrap'=>false,
+			//gvContainHeader($align,$width,$bColorHarga)
+			'headerOptions'=>Yii::$app->gv->gvContainHeader('center','50px',$bColorHarga),
+			'contentOptions'=>Yii::$app->gv->gvContainBody('left','50px',''),
+			
+		]	
+		,//HARI
+		[
+			'attribute'=>'PERIODE_TGL2',
+			'filterType'=>true,
+			'filterOptions'=>Yii::$app->gv->gvFilterContainHeader('0','50px'),
+			'hAlign'=>'right',
+			'vAlign'=>'middle',
+			'mergeHeader'=>false,
+			'noWrap'=>false,
+			//gvContainHeader($align,$width,$bColorHarga)
+			'headerOptions'=>Yii::$app->gv->gvContainHeader('center','50px',$bColorHarga),
+			'contentOptions'=>Yii::$app->gv->gvContainBody('left','50px',''),			
+		]	
+		,//PERIODE_TGL1
+		[
+			'attribute'=>'HARGA_JUAL',
+			'filterType'=>true,
+			'filterOptions'=>Yii::$app->gv->gvFilterContainHeader('0','50px'),
+			'hAlign'=>'right',
+			'vAlign'=>'middle',
+			'mergeHeader'=>false,
+			'noWrap'=>false,
+			//gvContainHeader($align,$width,$bColorHarga)
+			'headerOptions'=>Yii::$app->gv->gvContainHeader('center','50px',$bColorHarga),
+			'contentOptions'=>Yii::$app->gv->gvContainBody('left','50px',''),			
+		]	
+		,//PERIODE_TGL2
+		[
+			'attribute'=>'STATUS',
+			'filterType'=>true,
+			'filterOptions'=>Yii::$app->gv->gvFilterContainHeader('0','50px'),
+			'hAlign'=>'right',
+			'vAlign'=>'middle',
+			'mergeHeader'=>false,
+			'noWrap'=>false,
+			//gvContainHeader($align,$width,$bColorHarga)
+			'headerOptions'=>Yii::$app->gv->gvContainHeader('center','50px',$bColorHarga),
+			'contentOptions'=>Yii::$app->gv->gvContainBody('left','50px',''),			
+		]	
 	
+	];
+
+	$gvHargaPerStore=GridView::widget([
+		'id'=>'gv-harga-per-store',
+		'dataProvider' => $dataProvider,
+		'filterModel' => $searchModel,
+		'columns'=>$gvAttHarga,	
+		'pjax'=>true,
+		'pjaxSettings'=>[
+			'options'=>[
+				'enablePushState'=>false,
+				'id'=>'gv-harga-per-store',
+		    ],						  
+		],
+		'hover'=>true, //cursor select
+		'responsive'=>true,
+		'responsiveWrap'=>true,
+		'bordered'=>true,
+		'striped'=>true,
+		'autoXlFormat'=>true,
+		'export' => false,
+		'panel'=>false,
+		'toolbar' => false,
+		'panel' => [
+			//'heading'=>false,
+			//'heading'=>tombolBack().'<div style="float:right"> '.tombolCreate().' '.tombolExportExcel().'</div>',  
+			//'heading'=>tombolBack().' '.$pageNm,  
+			'type'=>'info',
+			//'before'=> tombolBack().'<div style="float:right"> '.tombolCreate().' '.tombolExportExcel().'</div>',
+			//'before'=> tombolBack(),
+			'before'=>false,
+			'showFooter'=>false,
+		],
+		
+		'summary'=>false,
+		'floatOverflowContainer'=>true,
+		'floatHeader'=>true,
+	]); 
+	
+?>
+<?=$gvHargaPerStore?>
